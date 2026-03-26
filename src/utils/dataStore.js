30786d51ebda3a2b7ca25633cf31e7445bc80c36
Darf <xiaoxian@axtn.net>
2026-03-26 21:21:34 +0800

    Initial commit

--- src/utils/dataStore.js
+++
@@ -1,9 +0,0 @@
-import { APP_CONFIG } from '../config'
-
-export const getProjects = () => {
-  return APP_CONFIG.projects?.list || []
-}
-
-export const getFriends = () => {
-  return APP_CONFIG.friends?.list || []
-}
